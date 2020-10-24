7072028a407dfd94e9f5310f233cced7236ba97f
Fernando Oliva <71294422+FernandoOliva18212205@users.noreply.github.com>
2020-10-24 08:21:48 -0700

    Create Programa19.cpp

---
+++ Programas/Programa19.cpp
@@ -0,0 +1,20 @@
+/*
+INSTITUTO TECNOLOGICO DE TIJUANA
+CARLOS FERNANDO LEAL OLIVA
+NO.CONTROL: 18212205
+*/
+//Programa 19. - Encontrar la suma de los enteros del 1 al 1000. 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() { int acum=0;
+
+for (int i = 1; i <= 1000; i++)
+{
+    acum += i;
+}
+
+cout << "La suma es: " + to_string(acum);
+}
