b06c7532e630b334af76f8eb69b60783e36d9b9f
Fernando Oliva <71294422+FernandoOliva18212205@users.noreply.github.com>
2020-10-24 08:22:48 -0700

    Create Programa20.cpp

---
+++ Programas/Programa20.cpp
@@ -0,0 +1,21 @@
+/*
+INSTITUTO TECNOLOGICO DE TIJUANA
+CARLOS FERNANDO LEAL OLIVA
+NO.CONTROL: 18212205
+*/
+//Programa 20.- Encontrar la suma de todos los enteros pares del 2 al 2000.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() { int acum = 0;
+
+for (int i = 2; i <= 2000; i=i+2)
+{
+    cout << to_string(i) + "\n";
+    acum += i;
+}
+
+cout << "La suma es: " + to_string(acum);
+}
