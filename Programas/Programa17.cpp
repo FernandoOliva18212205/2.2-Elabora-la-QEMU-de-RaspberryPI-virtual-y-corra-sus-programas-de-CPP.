4340548ec80a24f565e26048892b1339027f8108
Fernando Oliva <71294422+FernandoOliva18212205@users.noreply.github.com>
2020-10-24 08:18:53 -0700

    Create Programa17.cpp

---
+++ Programas/Programa17.cpp
@@ -0,0 +1,17 @@
+/*
+INSTITUTO TECNOLOGICO DE TIJUANA
+CARLOS FERNANDO LEAL OLIVA
+NO.CONTROL: 18212282
+*/
+//Programa 17. - Calcular e imprimir la suma de los enteros del 1 al 20. 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() { 
+int acum = 0; 
+for (int i = 1; i <= 20; i++) 
+{ acum += i; } 
+cout << acum << endl; 
+}
